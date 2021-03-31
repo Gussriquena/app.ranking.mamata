5855db9b03aa86e087c5fe92ee239dcabcf80308
Gustavo Riquena <gustavo_riquena1@outlook.com>
2021-03-31 20:59:13 -0300

    card listing parlamentares

--- src/components/Parlamentares.vue
+++ src/components/Parlamentares.vue
@@ -1,5 +1,18 @@
 <template>
-    <div>
+    <div id="ParlamentaresList">
+
+        <div class="row">
+            <div class="card" style="width: 15em;" v-for="deputado in parlamentares" v-bind:key="deputado">
+                <img class="card-img-top" :src="deputado.urlFoto" :alt="deputado.nome">
+                <div class="card-body">
+                    <h5 class="card-title">{{ deputado.nome }}</h5>
+                </div>
+            </div>
+        </div>
+
+        <ul v-for="deputado in parlamentares" v-bind:key="deputado">
+            <li>{{ deputado.id }}</li>
+        </ul>
     </div>
 </template>
 
